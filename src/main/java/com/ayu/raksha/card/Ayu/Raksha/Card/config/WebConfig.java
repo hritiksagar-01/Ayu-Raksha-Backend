9dccb81d240837a40e045c809becc02276e2d11f
hritiksagar01 <143049203hritiksagar01@users.noreply.github.com>
2026-04-30 14:01:31 +0530

    test

--- src/main/java/com/ayu/raksha/card/Ayu/Raksha/Card/config/WebConfig.java
+++ src/main/java/com/ayu/raksha/card/Ayu/Raksha/Card/config/WebConfig.java
@@ -10,8 +10,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                // Add the IP address of your frontend to the list
-                .allowedOrigins("http://localhost:3000", "http://192.168.1.243:3000", "http://3.111.47.30:3000","http://192.168.29.203:3000","http://172.19.128.1:3000/", "http://13.233.98.216","http://13.201.93.117", "https://ayuraksha.pllatinum.me")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://192.168.1.243:3000",
+                        "http://3.111.47.30:3000",
+                        "http://192.168.29.203:3000",
+                        "http://172.19.128.1:3000",
+                        "http://13.233.98.216:3000",
+                        "http://13.201.93.117:3000",
+                        "https://ayuraksha.pllatinum.me"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
